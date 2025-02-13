6f0389562e2e6ce0a16b1ba520298b6489efbb85
Glendeschema <30286680+Glendeschema@users.noreply.github.com>
2025-02-13 21:22:40 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "MenstrualTracker";
-$username = "root";
-$password = "";
+$host = "lesegostore.database.windows.net";
+$dbname = "AdventureWorksLT2022";
+$username = "lesego";
+$password = "Mpotu@2025";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
